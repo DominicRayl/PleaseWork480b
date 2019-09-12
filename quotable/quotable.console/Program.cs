127c4b3d0ff276baa348e6f1481877ef57729591
DominicRayl <drayl20@jcu.edu>
2019-09-12 17:25:25 -0400

    commit

---
+++ quotable/quotable.console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using quotable.core;
+
+namespace quotable.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleRandomQuoteProvider nw = new SimpleRandomQuoteProvider();
+            long n = long.Parse(Console.ReadLine());
+            IEnumerable<string> quotes = nw.quotez(n);
+
+            foreach (string s in quotes)
+            {
+                Console.WriteLine(s);
+            }
+            
+
+        }
+    }
+}
---
+++ quotable/quotable.core/SimpleRandomQuoteProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quotable.core
+{
+    public class SimpleRandomQuoteProvider : RandomQuoteProvider
+    {
+      
+        static void Main(string[] args)
+            {
+            
+
+            }
+
+        public IEnumerable<string> quotez(long numOfQuotes)
+           {
+            List<string> quoteList = new List<string>();
+            quoteList.Add("You can get straight A's and still flunk life.");
+            quoteList.Add("Never regret anything that made you smile.");
+            quoteList.Add("Just do it.");
+
+            if(numOfQuotes == 0)
+            {
+                quoteList.Remove("Hello World!");
+                quoteList.Remove("Never regret anything that made you smile.");
+                quoteList.Remove("Just do it.");
+            }
+
+            else if (numOfQuotes == 1)
+            {
+                quoteList.Remove("Never regret anything that made you smile.");
+                quoteList.Remove("Just do it.");
+            }
+            else if (numOfQuotes == 2)
+            {
+                quoteList.Remove("Just do it.");
+            }
+            else if (numOfQuotes > 3)
+            {
+                quoteList.Remove("Hello World!");
+                quoteList.Remove("Never regret anything that made you smile.");
+                quoteList.Remove("Just do it.");
+            }
+
+
+            return quoteList;
+
+            throw new NotImplementedException();
+           }
+    }
+}
